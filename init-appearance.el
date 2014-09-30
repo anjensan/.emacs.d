d9a247c3b3d8846ff9d47caeb02ab24ce0e1c91f
Andrei Zhlobich <a_zhlobich@wargaming.net>
2014-09-30 07:04:28 +0300

    Update cask-file

--- init-appearance.el
+++ init-appearance.el
@@ -1,8 +1,9 @@
-(defvar default-font "Source Code Pro 11")
+(set-default-font "Source Code Pro 11") 
+(set-fontset-font "fontset-default" '(#x0400 . #x04ff) "Dejavu Sans Mono-11") 
+
 (defvar default-frame-height 40 "Default frame height")
 (defvar default-frame-width 132 "Default frame width")
 
-(set-frame-font default-font)
 (set-frame-size (selected-frame) (- default-frame-width 1) default-frame-height)
 (load-theme 'base16-default t)
 
@@ -11,7 +12,6 @@
   "Hightliht 'self' keyword in python sources."
   :group 'font-lock-faces)
 
-(add-to-list 'default-frame-alist (cons 'font default-font))
 (add-to-list 'default-frame-alist (cons 'height (+ default-frame-height 0)))
 (add-to-list 'default-frame-alist (cons 'width (+ default-frame-width 1)))
 
