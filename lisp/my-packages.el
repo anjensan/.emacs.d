be16d2bbfb6637ba957434f64c9092d734d58a92
Andrei Zhlobich <a_zhlobich@wargaming.net>
2014-11-27 11:13:59 +0300

    Disable marmalade repo

--- lisp/my-packages.el
+++ lisp/my-packages.el
@@ -5,7 +5,6 @@
 
 (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (package-initialize)
 
 (defvar my-packages)
