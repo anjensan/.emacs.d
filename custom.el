6906a473873f1b6b69f9a1564c02b2edc2f870f1
Andrei Zhlobich <a_zhlobich@wargaming.net>
2015-01-10 12:13:47 +0300

    Update customs

--- custom.el
+++ custom.el
@@ -6,31 +6,18 @@
  '(delete-selection-mode t)
  '(eshell-banner-message "")
  '(inhibit-startup-screen t)
- '(initial-scratch-message
-   #(";; This buffer is for notes you don't want to save, and for Lisp evaluation.
-;; If you want to create a file, visit that file with Ctrl+O,
-;; then enter the text in that file's own buffer." 131 135
-						       (face ergoemacs-pretty-key)
-						       136 137
-						       (face ergoemacs-pretty-key)))
+ '(initial-scratch-message "")
  '(menu-bar-mode nil)
  '(org-CUA-compatible t)
  '(org-special-ctrl-a/e t)
  '(org-support-shift-select t)
- '(recentf-menu-before "Close")
  '(recentf-mode t)
- '(rm-blacklist
-   (quote
-    (" yas" " ||" " ErgoAJ[colemak]" " ErgoAJ[us]" " AC" " GitGutter" " ElDoc" " Undo-Tree" " MRev")))
+ '(rm-blacklist (quote (" yas" " ||" " ErgoAJ[colemak]" " ErgoAJ[us]" " AC" " GitGutter" " ElDoc" " Undo-Tree" " MRev")))
  '(scroll-bar-mode nil)
  '(scroll-error-top-bottom t)
  '(set-mark-command-repeat-pop t)
  '(show-paren-mode t)
- '(smex-prompt-string
-   #("Alt+A " 0 3
-     (face ergoemacs-pretty-key)
-     4 5
-     (face ergoemacs-pretty-key)))
+ '(smex-prompt-string "Alt+A ")
  '(tool-bar-mode nil)
  '(uniquify-buffer-name-style (quote post-forward) nil (uniquify)))
 
