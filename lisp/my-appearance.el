a68a8dcca0c024cdc5a986852f7f6f56d84d3e3a
Andrei Zhlobich <a_zhlobich@wargaming.net>
2015-06-18 17:52:21 +0300

    Disable base16 in termintal

--- lisp/my-appearance.el
+++ lisp/my-appearance.el
@@ -1,15 +1,16 @@
-(set-face-attribute 'default nil :font "Ubuntu Mono 12")
-
+(defvar default-frame-font "Ubuntu Mono 12" "Default frame font")
 (defvar default-frame-height 40 "Default frame height")
 (defvar default-frame-width 132 "Default frame width")
 
-(set-frame-size (selected-frame) (- default-frame-width 1) default-frame-height)
-(load-theme 'base16-default-dark t)
+(when (window-system)
+  (load-theme 'base16-default-dark t)
+  (add-to-list 'default-frame-alist (cons 'height (+ default-frame-height 0)))
+  (add-to-list 'default-frame-alist (cons 'width (+ default-frame-width 1)))
+  (set-face-attribute 'default nil :font default-frame-font)
+  (set-frame-size (selected-frame) (- default-frame-width 1) default-frame-height)
+  )
 
 (defface font-lock-self-class-face
   '((default (:foreground "light slate gray")))
   "Hightliht 'self' keyword in python sources."
   :group 'font-lock-faces)
-
-(add-to-list 'default-frame-alist (cons 'height (+ default-frame-height 0)))
-(add-to-list 'default-frame-alist (cons 'width (+ default-frame-width 1)))
